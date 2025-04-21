9d49d7f3c45bfb730b40f18e685680aa71e316aa
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-04-21 14:48:25 -0300

    ajuste de config para CORS

--- src/main.ts
+++ src/main.ts
@@ -5,33 +5,24 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuração do CORS
-  const allowedOrigins = [
-    'http://localhost:4200',
-    'https://use-tudu.com.br',
-    'http://localhost:3000',
-  ];
+  const isDev = process.env.NODE_ENV !== 'production';
 
-  app.enableCors({
-    origin: (origin, callback) => {
-      // Permite qualquer origem local e o domínio de produção
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
+  if (isDev) {
+    app.enableCors({
+      origin: [
+        'http://localhost:4200',
+        'http://localhost:3000',
+        'http://localhost:3001',
+      ],
+      credentials: true,
+    });
+  }
 
-  // Habilitar o ValidationPipe globalmente
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Remove campos não definidos no DTO
-      forbidNonWhitelisted: true, // Lança erro para campos não permitidos
-      transform: true, // Transforma os dados para os tipos esperados no DTO
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
