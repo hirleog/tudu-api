9f0608157d5764b826d5f3f3144d9df33c4c9abb
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-08-30 18:33:51 -0300

    metodos para alteração de senha

---
+++ src/cliente copy/dto/change-password.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, MinLength, Matches } from 'class-validator';
+
+export class ChangePasswordDto {
+  @IsString()
+  currentPassword: string;
+
+  @IsString()
+  @MinLength(8, { message: 'A nova senha deve ter pelo menos 8 caracteres' })
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+    message:
+      'A senha deve conter pelo menos uma letra maiúscula, uma minúscula e um número',
+  })
+  newPassword: string;
+
+  @IsString()
+  confirmNewPassword: string;
+}
